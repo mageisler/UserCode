62dcb7e5273c1b8b4b302bd3fa78a51c750a7117
mgeisler <>
2012-03-23 13:57:15 +0000

    Jet Corrections Service I

--- PF_PU_AssoMap/test/jetanlzr_cfg.py
+++ PF_PU_AssoMap/test/jetanlzr_cfg.py
@@ -1,75 +1,55 @@
 import FWCore.ParameterSet.Config as cms
 import sys, os
-import FWCore.ParameterSet.VarParsing as VarParsing
-options = VarParsing.VarParsing()
+	
+datasample = ['QCD','DYToMuMu','GluGluToHToGG']
+	
+spectra = ['15to30','30to50','50to80','80to120','120to170','170to300','300to470','470to600','600to800','800to1000']
+	
+	
+Outfile = "JetAnlzr"
+TightSelection=True
 
-allowedOptions = {}
+argBegin = 2
 
-options.register('spectra',
-	         '',
-		 VarParsing.VarParsing.multiplicity.singleton,
-	         VarParsing.VarParsing.varType.string,
-		 "Pt-spectra as add-on in the OutName"
-	        )
+if "jetanlzr_cfg.py" in str(sys.argv[0]):
+    argBegin=1
 		
-allowedOptions['spectra'] = ['15to30','30to50','50to80','80to120','120to170','170to300','300to470','470to600','600to800','800to1000','15to3000_Flat']
-
-options.register('datasample',
-	         '',
-		 VarParsing.VarParsing.multiplicity.singleton,
-	         VarParsing.VarParsing.varType.string,
-		 "Type of the datasample"
-	        )
+for i in range(len(datasample)):
+    if datasample[i] in str(sys.argv[argBegin]):
+        Outfile+= "_" + str(datasample[i])	
 		
-allowedOptions['datasample'] = ['QCD','DYToMuMu','GluGluToHToGG']
+for i in range(len(spectra)):
+    if spectra[i] in str(sys.argv[argBegin]):
+        Outfile+= "_Pt-" + str(spectra[i])	
+	
+if "False" in str(sys.argv[argBegin]):
+    TightSelection = False
+	    
+Outfile += ".root"
 
-options.register('tightSel',
-	         True,
-		 VarParsing.VarParsing.multiplicity.singleton,
-	         VarParsing.VarParsing.varType.bool,
-		 "Tracking Particles tight selection"
-	        ) 
-		
-options.parseArguments()
+print " Outfile set to " + Outfile
 
 process = cms.Process("JetAnlzr")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('file:/tmp/mgeisler/GluGluToHToGG_M-120_7TeV-pythia6.root')
+    fileNames = cms.untracked.vstring('file:/tmp/mgeisler/GluGluToHToGG_M-120_7TeV-pythia6.root'),
+    #eventsToProcess = cms.untracked.VEventRange('1:93299'),
 )
 		
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = cms.untracked.int32(3)
 )
-		
-Outfile = "JetAnlzr"
-
-if options.datasample:
-    if not str(options.datasample) in allowedOptions['datasample']:
-        print " " + str(options.datasample) + " cant be found in the allowed values for datasample"
-    else:
-	Outfile += "_" + str(options.datasample)
-        				
-if options.spectra:
-    if not str(options.spectra) in allowedOptions['spectra']:
-        print " " + str(options.spectra) + " cant be found in the allowed values for spectra"
-    else:
-	Outfile += "_Pt-" + str(options.spectra)
-	    
-Outfile += ".root"
  	
 process.TFileService = cms.Service('TFileService',
     fileName = cms.string(Outfile),
     closeFileFast = cms.untracked.bool(True),
 )
-
-print " Outfile set to " + Outfile
 		
 ### conditions
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = 'MC_44_V12::All'
+process.GlobalTag.globaltag = 'MC_44_V13::All'
 
 ### standard includes
 process.load('Configuration.StandardSequences.Services_cff')
@@ -93,28 +73,7 @@ from MGeisler.TrackValidator.TrackingParticleSelection_cfi import *
 
 ########### track selection configuration ########
 process.cutsRecoTracks.minRapidity = cms.double(-2.5)
-process.cutsRecoTracks.maxRapidity = cms.double(2.5)
-
-TightSelection = options.tightSel
-	
-
-if TightSelection:
-    print " Tight Track selection is used"
-    process.cutsRecoTracks.quality = cms.vstring('highPurity')
-    process.cutsRecoTracks.tip = cms.double(3.)
-    process.cutsRecoTracks.lip = cms.double(30.)
-    process.cutsRecoTracks.ptMin = cms.double(1.)
-else:
-    print " Loose Track selection is used"
-    process.cutsRecoTracks.quality = cms.vstring('highPurity','tight','loose')
-    process.cutsRecoTracks.tip = cms.double(120.)
-    process.cutsRecoTracks.lip = cms.double(280.)
-    process.cutsRecoTracks.ptMin = cms.double(0.1)
-    
-    TrackingParticleSelectionGeneral.tipTP = cms.double(120.0)
-    TrackingParticleSelectionGeneral.lipTP = cms.double(280.0)
-    TrackingParticleSelectionGeneral.ptMinTP = cms.double(0.1)
-    TrackingParticleSelectionGeneral.minHitTP = cms.int32(3)
+process.cutsRecoTracks.maxRapidity = cms.double(2.5)	
 
 ### AssociationMap-specific includes		
 from MGeisler.PF_PU_AssoMap.pf_pu_assomap_cfi import Tracks2Vertex
@@ -141,39 +100,7 @@ process.FirstVertexTrackCollectionAll = cms.EDProducer('FirstVertexTracks',
           VertexTrackAssociationMap = cms.InputTag('Tracks2VertexAll'),
 )
 
-########### configuration MultiTrackValidator 1 ########
-process.multiTrackValidator.outputFile = Outfile
-process.multiTrackValidator.associators = ['TrackAssociatorByHits']
-process.multiTrackValidator.skipHistoFit=cms.untracked.bool(False)
-process.multiTrackValidator.label = ['cutsRecoTracks','FirstVertexTrackCollection1st','FirstVertexTrackCollectionAll']
-process.multiTrackValidator.useLogPt=cms.untracked.bool(True)
-process.multiTrackValidator.minpT = cms.double(0.1)
-process.multiTrackValidator.maxpT = cms.double(3000.0)
-process.multiTrackValidator.nintpT = cms.int32(40)
-process.multiTrackValidator.UseAssociators = cms.bool(True)
-process.multiTrackValidator.runStandalone = cms.bool(True)
-				
-process.trackValidator = cms.EDAnalyzer('TrackValidator',
-	tcLabel = cms.VInputTag(cms.InputTag("cutsRecoTracks"),cms.InputTag("FirstVertexTrackCollection1st"),cms.InputTag("FirstVertexTrackCollectionAll"),),
-    	tcRefLabel = cms.InputTag("generalTracks"),
-    	PULabel = cms.InputTag("addPileupInfo"),
-    	TPLabel = cms.InputTag("mergedtruth","MergedTrackTruth"),
-	ignoremissingtrackcollection=cms.bool(False),
-	UseLogPt=cms.bool(False),
-	generalTpSelector = TrackingParticleSelectionGeneral,
-)
-	
-process.PFCandidates1st = cms.EDProducer('PFCand_NoPU_WithAM',
-	  PFCandidateCollection = cms.InputTag('particleFlow'),
-	  VertexCollection = cms.InputTag('offlinePrimaryVertices'),
-	  VertexTrackAssociationMap = cms.InputTag('Tracks2Vertex1st'),
-	  ConversionsCollection = cms.InputTag('allConversions'),
-	  V0KshortCollection = cms.InputTag('generalV0Candidates','Kshort'),
-	  V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
-	  NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
-)
-
-process.PFCandidatesAll = cms.EDProducer('PFCand_NoPU_WithAM',
+process.PFCandidatesAll1 = cms.EDProducer('PFCand_NoPU_WithAM',
 	  PFCandidateCollection = cms.InputTag('particleFlow'),
 	  VertexCollection = cms.InputTag('offlinePrimaryVertices'),
 	  VertexTrackAssociationMap = cms.InputTag('Tracks2VertexAll'),
@@ -181,19 +108,63 @@ process.PFCandidatesAll = cms.EDProducer('PFCand_NoPU_WithAM',
 	  V0KshortCollection = cms.InputTag('generalV0Candidates','Kshort'),
 	  V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
 	  NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
-)
-	
-from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets		
-process.kt6PFJets1st = kt4PFJets.clone(
-	src = cms.InputTag("PFCandidates1st"),
-	rParam = cms.double(0.6),
-	doAreaFastjet = cms.bool(True),
-	doRhoFastjet = cms.bool(True),
-	Ghost_EtaMax = cms.double(6.5),
+	  IpMin = cms.double(0.3),
 )
 
+process.PFCandidatesAll3 = cms.EDProducer('PFCand_NoPU_WithAM',
+	  PFCandidateCollection = cms.InputTag('particleFlow'),
+	  VertexCollection = cms.InputTag('offlinePrimaryVertices'),
+	  VertexTrackAssociationMap = cms.InputTag('Tracks2VertexAll'),
+	  ConversionsCollection = cms.InputTag('allConversions'),
+	  V0KshortCollection = cms.InputTag('generalV0Candidates','Kshort'),
+	  V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
+	  NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
+	  IpMin = cms.double(0.6),
+)
+
+process.PFCandidatesAll5 = cms.EDProducer('PFCand_NoPU_WithAM',
+	  PFCandidateCollection = cms.InputTag('particleFlow'),
+	  VertexCollection = cms.InputTag('offlinePrimaryVertices'),
+	  VertexTrackAssociationMap = cms.InputTag('Tracks2VertexAll'),
+	  ConversionsCollection = cms.InputTag('allConversions'),
+	  V0KshortCollection = cms.InputTag('generalV0Candidates','Kshort'),
+	  V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
+	  NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
+	  IpMin = cms.double(1.5),
+)
+
+process.PFCandidatesAll10 = cms.EDProducer('PFCand_NoPU_WithAM',
+	  PFCandidateCollection = cms.InputTag('particleFlow'),
+	  VertexCollection = cms.InputTag('offlinePrimaryVertices'),
+	  VertexTrackAssociationMap = cms.InputTag('Tracks2VertexAll'),
+	  ConversionsCollection = cms.InputTag('allConversions'),
+	  V0KshortCollection = cms.InputTag('generalV0Candidates','Kshort'),
+	  V0LambdaCollection = cms.InputTag('generalV0Candidates','Lambda'),
+	  NIVertexCollection = cms.InputTag('particleFlowDisplacedVertex'),
+	  IpMin = cms.double(3.),
+)
+				
+process.trackValidator = cms.EDAnalyzer('TrackValidator',
+	tcLabel = cms.VInputTag(cms.InputTag("cutsRecoTracks"),cms.InputTag("FirstVertexTrackCollection1st"),cms.InputTag("FirstVertexTrackCollectionAll")),
+    	tcRefLabel = cms.InputTag("generalTracks"),
+	pfLabel = cms.VInputTag(cms.InputTag("particleFlow"),cms.InputTag("PFCandidatesAll1"),cms.InputTag("PFCandidatesAll3"),cms.InputTag("PFCandidatesAll5"),cms.InputTag("PFCandidatesAll10")),
+    	pfRefLabel = cms.InputTag("particleFlow"),
+    	PULabel = cms.InputTag("addPileupInfo"),
+    	TPLabel = cms.InputTag("mergedtruth","MergedTrackTruth"),
+	ignoremissingtrackcollection=cms.bool(False),
+	photonPtMin=cms.double(1.0),
+	photonEtaMin=cms.double(-2.4),
+	photonEtaMax=cms.double(2.4),
+	photonLip=cms.double(30.),
+	photonTip=cms.double(3.),
+	UseLogPt=cms.bool(False),
+	generalTpSelector = TrackingParticleSelectionGeneral,
+)
+	
+from RecoJets.JetProducers.kt4PFJets_cfi import kt4PFJets	
+
 process.kt6PFJetsAll = kt4PFJets.clone(
-	src = cms.InputTag("PFCandidatesAll"),
+	src = cms.InputTag("PFCandidatesAll3"),
 	rParam = cms.double(0.6),
 	doAreaFastjet = cms.bool(True),
 	doRhoFastjet = cms.bool(True),
@@ -221,31 +192,6 @@ process.kt6GenJetsn = cms.EDProducer("CandViewNtpProducer",
 	),     
 )
 
-process.kt6PFJets1stn = cms.EDProducer("CandViewNtpProducer", 
-    src = cms.InputTag("kt6PFJets1st"),
-    lazyParser = cms.untracked.bool(True),
-    prefix = cms.untracked.string(""),
-    eventInfo = cms.untracked.bool(True),
-    variables = cms.VPSet(
-        cms.PSet(
-            tag = cms.untracked.string("pt"),
-            quantity = cms.untracked.string("pt")
-        ),
-        cms.PSet(
-            tag = cms.untracked.string("jetArea"),
-            quantity = cms.untracked.string("jetArea")
-        ),
-        cms.PSet(
-            tag = cms.untracked.string("eta"),
-            quantity = cms.untracked.string("eta")
-        ),
-        cms.PSet(
-            tag = cms.untracked.string("phi"),
-            quantity = cms.untracked.string("phi")
-        ),
-    )
-)
-
 process.kt6PFJetsAlln = cms.EDProducer("CandViewNtpProducer", 
     src = cms.InputTag("kt6PFJetsAll"),
     lazyParser = cms.untracked.bool(True),
@@ -271,14 +217,54 @@ process.kt6PFJetsAlln = cms.EDProducer("CandViewNtpProducer",
     )
 )
 
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
+from JetMETCorrections.Configuration.JetCorrectionProducers_cff import *
+
+process.kt6PFJetsAllJCS = cms.ESSource('LXXXCorrectionService',
+        level     = cms.string('L3Absolute'),
+        algorithm = cms.string('KT6PF'),
+        useCondDB = cms.untracked.bool(True),
+        era = cms.string(''),
+        section   = cms.string(''),
+) 
+
+process.kt6PFJetsAllJCP = cms.EDProducer('PFJetCorrectionProducer',
+        src        = cms.InputTag('kt6PFJetsAll'),
+        correctors = cms.vstring('kt6PFJetsAllJCS')
+) 
+    
 process.jetanalyzer = cms.EDAnalyzer('JetAnlzr',
 	genJets = cms.string("kt6GenJetsn"),
-	recoJets = cms.vstring("kt6PFJetsAlln","kt6PFJets1stn"),
-	PileUpInfo = cms.string("addPileupInfo")
+	recoJets = cms.vstring("kt6PFJetsAlln"),
+	PileUpInfo = cms.string("addPileupInfo"),
+	JetCorrector = cms.string("kt6PFJetsAllJC")
 )
+
 
 # paths	
 
+if TightSelection:
+    print " Tight Track selection is used"
+    process.cutsRecoTracks.quality = cms.vstring('highPurity')
+    process.cutsRecoTracks.tip = cms.double(3.)
+    process.cutsRecoTracks.lip = cms.double(30.)
+    process.cutsRecoTracks.ptMin = cms.double(1.)
+else:
+    print " Loose Track selection is used"
+    process.cutsRecoTracks.quality = cms.vstring('highPurity','tight','loose')
+    process.cutsRecoTracks.tip = cms.double(120.)
+    process.cutsRecoTracks.lip = cms.double(280.)
+    process.cutsRecoTracks.ptMin = cms.double(0.1)
+    
+    TrackingParticleSelectionGeneral.tip = cms.double(120.0)
+    TrackingParticleSelectionGeneral.lip = cms.double(280.0)
+    TrackingParticleSelectionGeneral.ptMin = cms.double(0.1)
+    TrackingParticleSelectionGeneral.minHit = cms.int32(3)
+    
+    process.trackValidator.photonPtMin=cms.double(0.1)
+    process.trackValidator.photonLip=cms.double(280.)
+    process.trackValidator.photonTip=cms.double(120.)
+    
 ### produce the association map
 process.T2V = cms.Sequence(
       process.Tracks2Vertex1st
@@ -291,46 +277,50 @@ process.FVTC = cms.Sequence(
     + process.FirstVertexTrackCollectionAll
 )
 
-### do the efficiency and fake rate analyses for the charged particles
-process.MTV = cms.Sequence(
-      #process.multiTrackValidator
-      process.trackValidator
-)
-
 ### produce a collection of PFCandidates associated to the first vertex
 process.PFCand = cms.Sequence(
-      process.PFCandidates1st
-    + process.PFCandidatesAll
+      process.PFCandidatesAll1
+    * process.PFCandidatesAll3
+    * process.PFCandidatesAll5
+    * process.PFCandidatesAll10
+)
+
+### do the efficiency and fake rate analyses for the charged particles
+process.MTV = cms.Sequence(
+      process.trackValidator
 )
 
 ### produce a jet collection from the PFCandidates
 process.PFJ = cms.Sequence(
-      process.kt6PFJets1st
-    + process.kt6PFJetsAll
+      process.kt6PFJetsAll
 )
 
 ### produce the edm nTuples from the jet collection
 process.nTs = cms.Sequence(
       process.kt6GenJetsn
-    + process.kt6PFJets1stn
     + process.kt6PFJetsAlln
 )
+
+### produce the jet correction services
+process.jcs = cms.Sequence(
+      process.kt6PFJetsAllJCP
+)
 		
-### do the jet analysis
-						  
+### do the jet analysis				  
 process.JA = cms.Sequence(
-      process.jetanalyzer
+     process.jetanalyzer
 )
 
 process.p = cms.Path(
       process.cutsRecoTracks    ### produce the track collection for the analysis of of the charged tracks
     * process.T2V		### produce the association map
-    * process.FVTC		### produce a collection of tracks associated to the first vertex
-    * process.MTV		### do the efficiency and fake rate analyses for the charged particles 	
-    #* process.PFCand		### produce a collection of PFCandidates associated to the first vertex	
-    #* process.PFJ		### produce a jet collection from the PFCandidates
-    #* process.nTs		### produce the edm nTuples from the jet collection
-    #* process.JA		### do the jet analysis	
+    * process.FVTC		### produce a collection of tracks associated to the first vertex	
+    * process.PFCand		### produce a collection of PFCandidates associated to the first vertex	
+    * process.MTV		### do the efficiency and fake rate analyses for the particles 
+    * process.PFJ		### produce a jet collection from the PFCandidates
+    * process.nTs		### produce the edm nTuples from the jet collection
+    * process.jcs		### produce the jet correction services
+    * process.JA		### do the jet analysis	
 )
 
 process.schedule = cms.Schedule(
