05208090586df996611ef067d4bc3ea754ec91b2
mgeisler <>
2012-07-25 12:27:20 +0000

    Kill all unneccessary files

--- PF_PU_AssoMap/python/PF_PU_AssoMap_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from DQMServices.Core.DQM_cfg import *
-DQMStore.collateHistograms =cms.untracked.bool(True)
-from MGeisler.PF_PU_AssoMap.pf_pu_assomap_cfi import *
--- PF_PU_AssoMap/python/cuts_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from MGeisler.PF_PU_AssoMap.cutsRecoTracks_cfi import *
-
-
