a1b838924dd143a4c9dc47dbe40d2eb8b69886d9
mgeisler <>
2012-08-13 10:21:16 +0000

    Update and creation actually

--- TrackValidator/python/trackvalidator_cfi.py
+++ TrackValidator/python/trackvalidator_cfi.py
@@ -1,4 +1,23 @@
 import FWCore.ParameterSet.Config as cms
 
-demo = cms.EDAnalyzer('TrackValidator'
+from MGeisler.TrackValidator.TrackingParticleSelection_cfi import *
+
+trackvalidatorDemo = cms.EDAnalyzer('TrackValidator',
+	tcLabel = cms.VInputTag(cms.InputTag("generalTracks")),
+    	tcRefLabel = cms.InputTag("generalTracks"),
+	pfLabel = cms.VInputTag(),
+    	pfRefLabel = cms.InputTag("particleFlow"),
+    	PULabel = cms.InputTag("addPileupInfo"),
+    	TPLabel = cms.InputTag("mergedtruth","MergedTrackTruth"),
+	ignoremissingtrackcollection=cms.bool(False),
+	photonPtMin=cms.double(1.0),
+	photonEtaMin=cms.double(-2.4),
+	photonEtaMax=cms.double(2.4),
+	photonLip=cms.double(30.),
+	photonTip=cms.double(3.),
+	UseLogPt=cms.bool(False),
+	generalTpSelector = TrackingParticleSelectionGeneral,
+	useJetWeighting = cms.untracked.bool(True),
+	genJetCollLabel = cms.untracked.string("ak5GenJets"),
+	jetCollLabel = cms.untracked.string("ak5PFJets"),
 )
